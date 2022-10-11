603d6825cddbf441de631f41ecc330bb301fca15
new_user <nrustgen@gmail.com>
2022-10-10 18:28:31 -0700

    2. Domain model of the "AutoMag" project [#504897]

---
+++ db/scripts/update_001.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS auto_user (
+   id SERIAL PRIMARY KEY,
+   login VARCHAR,
+   password VARCHAR
+);
---
+++ db/scripts/update_002.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS auto_post (
+   id SERIAL PRIMARY KEY,
+   text TEXT,
+   created TIMESTAMP,
+   auto_user_id INT
+);
